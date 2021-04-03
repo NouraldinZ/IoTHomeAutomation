abd07ec3e5fe2625ad68fa81611327cc8991871c
Rithik Sowdermett <rsowd046@uottawa.ca>
2021-04-03 01:03:39 -0400

    Added basic UI on Lights component

--- src/Components/Navigation/HomeAutomation.js
+++ src/Components/Navigation/HomeAutomation.js
@@ -2,13 +2,15 @@ import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import Dashboard from "../../Views/Dashboard";
 import Settings from "../../Views/Settings";
+import Lights from "../../Views/Lights"
 
 export default createStackNavigator(
   {
     Dashboard,
     Settings,
+    Lights,
   },
   {
-    initialRouteName: "Dashboard",
+    initialRouteName: "Lights",
   }
 );
--- src/Views/Lights.js
+++ src/Views/Lights.js
@@ -14,13 +14,32 @@ const styles = StyleSheet.create({
 });
 
 class Lights extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lightsOn: false,
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.toggleLight = this.toggleLight.bind(this);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>I'm the MyComponent component</Text>
+        <h3>Light Status: {this.state.lightsOn ? 'ON' : 'OFF'}</h3>
+        <button onClick={this.toggleLight}>
+          Toggle Light Switch
+        </button>
       </View>
     );
   }
-}
 
+  toggleLight(){
+    console.log(!this.state.lightsOn);
+    this.setState(state => ({
+      lightsOn: !state.lightsOn
+    }));
+    // TODO: Send request to Raspberry-Pi to toggle lights switch
+  }
+}
 export default Lights;
