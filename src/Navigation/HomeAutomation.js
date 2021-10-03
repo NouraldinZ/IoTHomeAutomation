596e12fbbfd07fe0bd6472ef12b70ad8707578f1
Nour Zeina <43045885+NouraldinZ@users.noreply.github.com>
2021-10-03 04:05:55 -0600

    added the lights screen to routing

--- src/Navigation/HomeAutomation.js
+++ src/Navigation/HomeAutomation.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
 import Dashboard from '../Views/Dashboard';
 import Settings from '../Views/Settings';
+import Lights from '../Views/Lights';
 
 export default createStackNavigator(
   {
     Dashboard,
     Settings,
+    Lights,
   },
   {
     initialRouteName: 'Dashboard',
