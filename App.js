d425766ff0de8310e8d73ebcfd7aae2e72864247
Nour Zeina <43045885+NouraldinZ@users.noreply.github.com>
2021-10-03 02:52:39 -0600

    updating libraries and imports

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { createAppContainer } from "react-navigation";
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
 
-import HomeAutomation from "./src/Components/Navigation/HomeAutomation";
+import HomeAutomation from './src/Navigation/HomeAutomation';
 
 export default createAppContainer(HomeAutomation);
